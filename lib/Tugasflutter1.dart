97921d19c4d3a1205c9a4ec66da569a22700adfe
ariyanto93045-source <ariyanto93045@gmail.com>
2026-05-08 08:56:34 +0700

    Tugasflutter1

---
+++ lib/Tugasflutter1.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class tugas1flutter extends StatelessWidget {
+  const tugas1flutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profil Saya")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Nama Lengkap
+            Row(
+               mainAxisAlignment: MainAxisAlignment.center,// untuk membungkus nama lengkap agar di tengah 
+              children: [
+                Text(
+                  'Ariyanto',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+
+            SizedBox(height: 15),
+
+            // Kota dengan Icon
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: const Color.fromARGB(255, 120, 54, 244),
+                ),
+
+                SizedBox(width: 5),
+
+                Text(
+                  'Jakarta',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 15),
+
+            // Deskripsi Singkat
+            Text(
+              'Saya adalah peserta pelatihan yang sedang belajar Flutter dan pengembangan aplikasi mobile.',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+                
+              ),
+              textAlign: TextAlign.center ,
+              
+            ),
+        
+          
+        ],
+      ),
+    );
+  }
+}
